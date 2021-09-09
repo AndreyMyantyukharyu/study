d6c655af242dbb66ea9503a237480b89b8b5d4ce
Andrey <andreym.up@gmail.com>
2021-09-09 21:59:35 +0300

    MovieDB

---
+++ js/script2.js
@@ -0,0 +1,22 @@
+const numberOfFilms = +prompt('How many movies have you seen?', '');
+
+const personalMovieDB = {
+
+    count: numberOfFilms,
+    
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+
+};
+
+const a = prompt('One of the last movies you watched?', ''),
+      b = prompt('How much would you rate the film?', ''),
+      c = prompt('One of the last movies you watched?', ''),
+      d = prompt('How much would you rate the film?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
